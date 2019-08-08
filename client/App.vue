66b41e68cf947eca1cf63ba2e8bed4f97d2e92b9
csthink <security.2009@live.cn>
2019-08-08 17:51:00 +0800

    添加 bootstrap 的支持

--- client/App.vue
+++ client/App.vue
@@ -1,15 +1,73 @@
 <template>
-  <div id="app">
-    {{ text }}
+  <div>
+    <b-alert show>
+      Default Alert
+    </b-alert>
+
+    <b-alert
+      variant="success"
+      show
+    >
+      Success Alert
+    </b-alert>
+
+    <b-alert
+      v-model="showDismissibleAlert"
+      variant="danger"
+      dismissible
+    >
+      Dismissible Alert!
+    </b-alert>
+
+    <b-alert
+      :show="dismissCountDown"
+      dismissible
+      variant="warning"
+      @dismissed="dismissCountDown=0"
+      @dismiss-count-down="countDownChanged"
+    >
+      <p>This alert will dismiss after {{ dismissCountDown }} seconds...</p>
+      <b-progress
+        variant="warning"
+        :max="dismissSecs"
+        :value="dismissCountDown"
+        height="4px"
+      />
+    </b-alert>
+
+    <b-button
+      variant="info"
+      class="m-1"
+      @click="showAlert"
+    >
+      Show alert with count-down timer
+    </b-button>
+    <b-button
+      variant="info"
+      class="m-1"
+      @click="showDismissibleAlert=true"
+    >
+      Show dismissible alert ({{ showDismissibleAlert ? 'visible' : 'hidden' }})
+    </b-button>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      text: 'Hello World!'
+  export default {
+    data() {
+      return {
+        dismissSecs: 10,
+        dismissCountDown: 0,
+        showDismissibleAlert: false
+      }
+    },
+    methods: {
+      countDownChanged(dismissCountDown) {
+        this.dismissCountDown = dismissCountDown
+      },
+      showAlert() {
+        this.dismissCountDown = this.dismissSecs
+      }
     }
   }
-}
 </script>
--- client/index.js
+++ client/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import BootstrapVue from 'bootstrap-vue'
+import './assets/style/global.scss'
+
+Vue.use(BootstrapVue)
+
 new Vue({
   render: h => h(App)
 }).$mount('#root');
