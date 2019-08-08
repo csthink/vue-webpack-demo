74d53c798065c8a9f8535fc297dc6cccb1baf29b
csthink <security.2009@live.cn>
2019-08-08 17:17:32 +0800

    webpack 支持 css,scss,image,font 的资源打包

--- build/webpack.client.js
+++ build/webpack.client.js
@@ -4,32 +4,44 @@ const webpackMerge = require('webpack-merge')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
-// const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin  = require('html-webpack-inline-source-plugin')
 
-const isDev = process.env.NODE_ENV === 'development'
+const devMode = process.env.NODE_ENV !== 'production'
 const webpackCommonConfig = require('./webpack.common')
 
 const defaultPluginConfig = [
+  // 创建一些在编译时可以配置的全局常量
   new webpack.DefinePlugin({
     'process.env': {
-      NODE_ENV: isDev ? '"development"' : '"production"'
+      NODE_ENV: devMode ? '"development"' : '"production"'
     }
   }),
   new VueLoaderPlugin(),
+  // 打包结束后，自动生成一个html文件，并把打包生成的js自动引入到这个html文件中
   new HtmlWebpackPlugin({
-    template: path.join(__dirname, "template.html")
+    template: path.join(__dirname, "template.html"), // 以 template.html为模板，把打包生成的js自动引入到这个html文件中
+    // inlineSource: '.(js|css)$' // css 或 js 文件内嵌到 html 页面中
+    inlineSource: 'runtime~.+\\.js'
   }),
-  new CleanWebpackPlugin()
+  new HtmlWebpackInlineSourcePlugin()
 ]
 
+// plugin 可以在 webpack 运行到某个时刻的时候，帮你做一些事情
 const devConfig = {
-  devtool: '#cheap-module-eval-source-map',
+  entry: {
+    app: path.resolve(__dirname, '../client/index.js')
+  },
+  devtool: 'cheap-module-eval-source-map', // sourcemap 打包编译后的文件和源文件的映射关系，用于开发者调试用
+  // 解决每次在src里编写完代码都需要手动重新运行 npm run dev
   devServer: {
-    port: 8000,
-    host: '0.0.0.0',
+    port: 8000, // 开发服务器监听的端口
+    host: '0.0.0.0', // 开发服务器监听的主机地址
     overlay: {
-      errors: true,
+      errors: true, // 在浏览器弹出提示有错误
     },
     hot: true,
     historyApiFallback:{
@@ -37,30 +49,49 @@ const devConfig = {
     }
   },
   plugins: defaultPluginConfig.concat([
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(), // 使用模块热更新插件
   ]),
   optimization: {
-    splitChunks: {
+    splitChunks: {  // 启动代码分割，有默认配置项
       chunks: 'all'
     }
   }
 }
 
 const prodConfig = {
+  entry: {
+    app: path.resolve(__dirname, '../client/index.js')
+  },
   output: {
-    chunkFilename: 'js/chunk.[chunkhash:8].js',
+    filename: 'js/[name][hash:8].js', // palceholder占位符,入口文件打包后生成的文件名
+    chunkFilename: 'js/vendor.[chunkhash:8].js', // 配置打包后这些第三方库的名字
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '' // 将注入到 html中的 js文件前面加上地址
   },
   devtool: 'cheap-module-source-map',
   plugins: defaultPluginConfig.concat([
-
+    new CleanWebpackPlugin(), // 在打包之前，可以删除dist文件夹下的所有内容
+    new MiniCssExtractPlugin({
+      filename: 'css/style.[contenthash:8].css',
+      chunkFilename: 'css/[name].chunk.[chunkhash:8].css'
+    })
   ]),
   optimization: {
-    runtimeChunk: true
+    splitChunks: { // 启动代码分割，有默认配置项
+      chunks: 'all'
+    },
+    minimizer: [
+      new TerserPlugin(), // JS 压缩
+      new OptimizeCSSAssetsPlugin({}) // CSS 压缩
+    ],
+    runtimeChunk: {
+      name: 'runtime'
+		}
   }
 }
 
 module.exports = () => {
-  if (isDev) {
+  if (devMode) {
     return webpackMerge(webpackCommonConfig, devConfig)
   } else {
     return webpackMerge(webpackCommonConfig, prodConfig)
--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,28 +1,89 @@
 const path = require('path')
 
-const isDev = process.env.NODE_ENV === 'development'
+const devMode = process.env.NODE_ENV !== 'production'
 const createVueLoaderOptions = require('./vue-loader.config')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: process.env.NODE_ENV || 'production',
   target: 'web',
-  entry: path.resolve(__dirname, '../client/index.js'),
   output: {
-    filename: 'js/bundle.[hash:8].js',
+    filename: 'js/[name].js', // palceholder占位符,入口文件打包后生成的文件名
+    chunkFilename: 'js/[name].js', // 异步加载的间接的js文件。用来打包import('module')方法中引入的模块
     path: path.resolve(__dirname, '../dist'),
-    publicPath: ''
+    publicPath: '' // 将注入到 html中的 js文件前面加上地址
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
         loader: 'vue-loader',
-        options: createVueLoaderOptions(isDev),
+        options: createVueLoaderOptions(devMode),
       },
       {
         test: /\.js$/,
+        loader: 'babel-loader',
         exclude: /node_modules/,
-        use: 'babel-loader'
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        oneOf: [
+          {
+            resourceQuery: /module/,
+            use: [
+              // loader的执行顺序是从右向左，从下到上
+              devMode ? 'vue-style-loader': MiniCssExtractPlugin.loader, // 将 JS 字符串生成为 style 节点
+              {
+                loader: 'css-loader', // 将 CSS 转化成 CommonJS 模块
+                options: {
+                  modules: {
+                    mode: 'local',
+                    localIdentName: '[path][name]__[local]',
+                  },
+                  importLoaders: 2, //如果sass文件里还引入了另外一个sass文件，另一个文件还会从sass-loader向上解析。如果不加，就直接从css-loader开始解析
+                  sourceMap: true,
+                  localsConvention: 'camelCase'
+                },
+              },
+              'postcss-loader', // 配置在 css-loader后，在sass|less|stylus-loader 之前
+              'sass-loader' // 将 Sass 编译成 CSS，默认使用 Node Sass
+            ]
+          },
+          {
+            use: [
+              devMode ? 'vue-style-loader': MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 2,
+                }
+              },
+              'postcss-loader',
+              'sass-loader'
+            ]
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name].[hash:8].[ext]',
+            outputPath: 'images/',
+            limit: 10240, // 图片小于limit(单位byte, 1KB)的时候会把图片BASE64编码,大于就会打包成文件格式
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'font/[name].[hash:8].[ext]',
+          },
+        }],
       }
     ]
   }
